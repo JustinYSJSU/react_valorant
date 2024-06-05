ca038ef6524bfe96af43b81a872a189d12a3d4e4
JustinYSJSU <justin.yamamoto@sjsu.edu>
2024-06-04 18:21:05 -0700

    Starting video uploads

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Home } from './pages/Home';
+import { Upload } from './pages/Upload';
+
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,7 @@ function App() {
           <Route path = "/" element={<Login />} />
           <Route path = "/register" element={<Register />} />
           <Route path = "/home" element={<Home />} />
+          <Route path = "/upload" element={<Upload />} />
         </Routes>
         
       </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
 import { auth } from "../config/firebase"
 import { signOut } from "firebase/auth"
 import { useNavigate } from "react-router-dom"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import HomeCSS from "../css/home.module.css"
-import { Placeholder } from "react-bootstrap"
+import { collection, getDocs, query, where, orderBy } from "firebase/firestore"
+import { db } from "../config/firebase"
 
 export const Home = () =>{
     const currentUser = auth.currentUser
     const navigate = useNavigate()
-    const [modal, setModal] = useState(false)
 
     const [agent, setAgent] = useState()
     const [map, setMap] = useState()
     const [result, setResult] = useState()
     const [time, setTime] = useState()
 
-    console.log(currentUser)
+    const [userVods, setUserVods] = useState([])
 
     const handleSignOut = () =>{
       signOut(auth).then( () =>{
@@ -24,6 +24,18 @@ export const Home = () =>{
         console.log(error)
       })
     }
+
+    useEffect( () => {
+      const getUserVods = async () =>{
+         const q = query(collection(db, "videos"), where ("user_id", "==", currentUser.uid))
+         const querySnapshot = await getDocs(q)
+         querySnapshot.forEach( (doc) => {
+          userVods.push(doc)
+         })
+      }
+      getUserVods()
+    }, [])
+
     return(
         <div className={HomeCSS['home-container']}>
             <div className={HomeCSS['home-top']}> 
@@ -32,15 +44,47 @@ export const Home = () =>{
             </div>
             
             <div className={HomeCSS['vod-options']}>
-
               <form>
-                <select class="selectpicker" className={HomeCSS['select-option-first']} >
+                <select className={HomeCSS['select-option-first']} >
                   <option value="non"> Agent </option>
-                  
+                  <option value="Astra"> Astra </option>
+                  <option value="Breach"> Breach </option>
+                  <option value="Brimstone"> Brimstone </option>
+                  <option value="Chamber"> Chamber </option>
+                  <option value="Clove"> Clove </option>
+                  <option value="Cypher"> Cypher </option>
+                  <option value="Deadlock"> Deadlock</option>
+                  <option value="Fade"> Fade </option>
+                  <option value="Gekko"> Gekko </option>
+                  <option value="Harbor"> Harbor </option>
+                  <option value="Iso"> Iso </option>
+                  <option value="Jett"> Jett </option>
+                  <option value="KAY/O"> KAY/O </option>
+                  <option value="Killjoy"> Killjoy </option>
+                  <option value="Neon"> Neon </option>
+                  <option value="Omen"> Omen </option>
+                  <option value="Phoenix"> Phoenix </option>
+                  <option value="Raze"> Raze </option>
+                  <option value="Reyna"> Reyna</option>
+                  <option value="Sage"> Sage </option>
+                  <option value="Skye"> Skye </option>
+                  <option value="Sova"> Sova </option>
+                  <option value="Viper"> Viper </option>
+                  <option value="Yoru"> Yoru </option>
                 </select>
 
                 <select className={HomeCSS['select-option']}>
                   <option value="non"> Map </option>
+                  <option value="Ascent"> Ascent </option>
+                  <option value="Bind"> Bind </option>
+                  <option value="Breeze"> Breeze </option>
+                  <option value="Fracture"> Fracture </option>
+                  <option value="Haven"> Haven </option>
+                  <option value="Icebox"> Icebox </option>
+                  <option value="Lotus"> Lotus </option>
+                  <option value="Pearl"> Pearl </option>
+                  <option value="Split"> Split </option>
+                  <option value="Sunset"> Sunset </option>
                 </select>
 
                 <select className={HomeCSS['select-option']}>
@@ -56,19 +100,12 @@ export const Home = () =>{
                 </select>
 
                </form>
-               <button className={HomeCSS['upload-button']} onClick={() => setModal(true)}> UPLOAD </button>
+               <button className={HomeCSS['upload-button']} onClick={() => navigate("/upload")}> UPLOAD </button>
             </div>
             
-
-            {modal && (
-             <div className={HomeCSS['upload-modal']}>
-                 <h1 className={HomeCSS['upload-modal-header']}>
-                    <p className={HomeCSS['upload-modal-title']}> Upload VOD </p>
-                    <button className={HomeCSS['upload-modal-cancel']} onClick={() => setModal(false)}> x </button>
-
-                 </h1>
-             </div>
-            )}
+            <div className={HomeCSS['vod-feed']}>
+               {userVods.length === 0 ? <h2> You have no VODS to review. Upload some! </h2> : <div> </div>}
+            </div>
         </div>
 
         
