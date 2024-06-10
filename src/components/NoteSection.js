2dee6dc60cc8039a464f54f1f99ac827c722fb6e
JustinYSJSU <justin.yamamoto@sjsu.edu>
2024-06-10 16:54:20 -0700

    Ability to add notes with tags to vod things to fix: clicking timestamp, ordering and filtering notes

--- src/components/NoteSection.js
+++ src/components/NoteSection.js
@@ -1,9 +1,11 @@
 import NoteSectionCSS from "../css/note_section.module.css"
 
-export const NoteSection = () =>{
+export const NoteSection = (props) =>{
+    //get all notes from the specified VOD_ID
     return(
         <div className={NoteSectionCSS['notes-display']}>
-            NOTES SECTION
+            {props.vodID}
+            {props.vodTimestamp}
         </div>
     )
 }
--- src/pages/Vod.js
+++ src/pages/Vod.js
@@ -1,19 +1,26 @@
 import VodCSS from "../css/vod.module.css"
 import { useParams } from "react-router-dom"
 import { useEffect, useRef, useState } from "react"
-import { collection, getDocs, query, where, doc } from "firebase/firestore"
-import { db } from "../config/firebase"
+import { collection, getDocs, query, where, addDoc} from "firebase/firestore"
+import { db,auth } from "../config/firebase"
 import { format, intervalToDuration } from "date-fns"
 import { NoteSection} from "../components/NoteSection"
 
 export const Vod = () =>{
     const {vodID} = useParams()
     const [vod, setVod] = useState()
-    const[notes, setNotes] = useState("notes here")
+    const [notes, setNotes] = useState([])
+
+    const [addingNote, setAddingNote] = useState(false)
+    const [addNoteButton, setAddNoteButton] = useState(false)
+    const [comment, setComment] = useState()
 
     const vodRef = useRef(0)
     const [vodTimestamp, setVodTimestamp] = useState()
     
+    const [tagFilter, setTagFilter] = useState()
+    const [tag, setTag] = useState()
+
     const convertTimestamp = (timestamp) =>{
         const ms = (timestamp.seconds) * 1000 + (timestamp.nanoseconds / 1000000)
         return format(new Date(ms), "MMMM dd, yyyy hh:mm a")
@@ -26,9 +33,43 @@ export const Vod = () =>{
       return `${zeroPad(minutes)}:${zeroPad(seconds)}`;
     }
 
+    const handleNote = async (e) => {
+      e.preventDefault();
+      const notesRef = collection(db, "notes");
+      try {
+        const currentUser = auth.currentUser;
+        await addDoc(notesRef, {
+          comment: comment,
+          tag: tag,
+          timestampConverted: convertPausedTimestamp(vodTimestamp),
+          timestamp: vodTimestamp,
+          vod_id: vodID,
+          uid: currentUser.uid,
+        });
+        console.log("NOTE ADDED");
+  
+        const copyOfNotes = [];
+        const q = query(collection(db, "notes"), where("vod_id", "==", vodID));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          copyOfNotes.push(doc.data());
+        });
+        setNotes(copyOfNotes);
+        setAddingNote(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const handlePlay = () =>{
+       setVodTimestamp(null)
+       setAddingNote(false)
+    }
+
     const handlePause = () =>{
        if(vodRef.current){
         setVodTimestamp(vodRef.current.currentTime)
+        setAddNoteButton(true)
        }
     }
 
@@ -51,6 +92,27 @@ export const Vod = () =>{
         getVod()
     },[])
 
+    useEffect(() => {
+      const getNotes = async () => {
+        const copyOfNotes = [];
+        try {
+          const q = query(collection(db, "notes"), where("vod_id", "==", vodID));
+          const querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
+            copyOfNotes.push(doc.data());
+          });
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setNotes(copyOfNotes);
+        }
+      };
+  
+      getNotes();
+    }, [vodID]);
+
+    console.log(notes)
+    console.log(notes.length)
     return(
         <div className={VodCSS['vod-page-container']}>
             <div className={VodCSS['vod-page-top']}> 
@@ -61,7 +123,7 @@ export const Vod = () =>{
               {!vod && <div> Loading VOD... </div>}
               {vod && 
               <div className={VodCSS['vod-display']}>
-                <video ref={vodRef} onPause={handlePause} onPlay={() => setVodTimestamp(null)}className={VodCSS['vod']} width="60%" height="60%" controls preload="metadata"> 
+                <video ref={vodRef} onPause={handlePause} onPlay={handlePlay}className={VodCSS['vod']} width="60%" height="60%" controls preload="metadata"> 
                       <source src={vod.video_url + "#t=0.1"} type="video/mp4"/>
                 </video>
 
@@ -79,12 +141,81 @@ export const Vod = () =>{
                 </div>
 
                 
-                {vodTimestamp && <div onClick={goToTimestamp(vodTimestamp)}> {convertPausedTimestamp(vodTimestamp)} </div>}
+                {vodTimestamp && 
+                 <div className={VodCSS['add-note']}> 
+                   <button className={VodCSS['add-note-button']} onClick={() => {setAddingNote(true)}}> ADD NOTE </button>
+                 </div>}
               </div>}
               
+              <div className={VodCSS['notes-display']}>
+                <select onChange={(e) => setTagFilter(e.target.value)} className={VodCSS['select-tag']}>
+                  <option value=""> Filter by... </option>
+                  <option value="Mechanics"> Mechanics </option>
+                  <option value="Communication"> Communication </option>
+                  <option value="Awareness"> Awareness </option>
+                  <option value="Utility"> Utility </option>
+                  <option value="Other"> Other </option>
+                </select>
 
-              {!notes && <div> Loading notes... </div>}
-              <NoteSection />
+                <div className={VodCSS['all-notes']}>
+                  {addingNote &&
+                     <form onSubmit={handleNote} className={VodCSS['add-note-form']}>
+                       <select onChange={(e) => setTag(e.target.value)} className={VodCSS['select-tag-second']} required="true">
+                         <option value=""> Tag... </option>
+                         <option value="mechanics"> Mechanics </option>
+                         <option value="communication"> Communication </option>
+                         <option value="awareness"> Awareness </option>
+                         <option value="utility"> Utility </option>
+                         <option value="other"> Other </option>
+                       </select>
+
+                       <textarea onChange={ (e) => setComment(e.target.value)} required="true" className={VodCSS['note-write']} placeholder="Note here..." />
+                       <button onClick={() => setAddingNote(false)} className={VodCSS['note-cancel']}> Cancel </button>
+                       <button type="submit" className={VodCSS['note-confirm']}> Submit </button>
+                     </form>
+                   }
+
+                  <div className={VodCSS['user-notes']}>
+                    {notes.length !== 0 ? (
+                    notes.map((note) => (
+                    <div className={VodCSS['note']}> 
+                       <a onClick={goToTimestamp(note.timestamp)} className={VodCSS['note-timestamp-link']}> <h3 className={VodCSS['note-timestamp']}> {note.timestampConverted} </h3> </a>
+                       <p className={VodCSS['note-comment']}> {note.comment} </p>
+                       {note.tag === 'mechanics' && 
+                       <div className={VodCSS['tag-mechanics']}>
+                         Mechanics
+                       </div>}
+
+                       {note.tag === 'communication' && 
+                       <div className={VodCSS['tag-communication']}>
+                         Communication
+                       </div>}
+
+                       {note.tag === 'awareness' && 
+                       <div className={VodCSS['tag-awareness']}>
+                         Awareness
+                       </div>}
+
+                       {note.tag === 'utility' && 
+                       <div className={VodCSS['tag-utility']}>
+                         Utility
+                       </div>}
+
+                       {note.tag === 'other' && 
+                       <div className={VodCSS['tag-other']}>
+                         Other
+                       </div>}
+
+                    </div>
+                    ))
+                    ) : (
+                   <div>No notes available</div>
+                   )}
+                  </div>
+                   
+
+                </div>
+              </div>
             </div>
             
         </div>
